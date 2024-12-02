40ff5f5deadda4df783cabc7f6631f9a61891136
Dario Hett <dariohett+github@gmail.com>
2024-12-02 19:25:07 +0100

    Day 02: problem 1 solved

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -3,13 +3,17 @@ use aoc24::*;
 use std::clone::Clone;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::str::FromStr;
 
-const DAY: &str = "02"; //
+const DAY: &str = "02";
 const INPUT_FILE: &str = "input/02.txt";
 
-const TEST: &str = "\
-<TEST-INPUT>
-"; // TODO: Add the test input
+const TEST: &str = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -18,13 +22,16 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().flatten().count();
-        Ok(answer)
+        let answer = reader.lines().map(|line| {
+            line.unwrap()
+                .split(' ')
+                .map(i32::from_str)
+                .fold(Day02State::Initial, |acc, v| day02_is_safe(acc, v.unwrap()))
+        }).collect::<Vec<Day02State>>();
+        Ok(answer.len()-answer.iter().map(|i| i.eq(&Day02State::Unsafe) as usize).sum::<usize>())
     }
 
-    // TODO: Set the expected answer for the test input
-    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+    assert_eq!(2, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = part1(input_file)?;
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,41 @@ pub fn start_day(day: &str) {
 }
 
 // Additional common functions
-
+#[derive(Ord, PartialOrd, PartialEq, Eq, Debug)]
+pub enum Day02State {
+    Initial,
+    FirstStep(i32),
+    Increasing(i32),
+    Decreasing(i32),
+    Unsafe,
+}
+pub fn day02_is_safe(acc: Day02State, v: i32) -> Day02State {
+    match acc {
+        Day02State::Unsafe => Day02State::Unsafe,
+        Day02State::Initial => return Day02State::FirstStep(v),
+        Day02State::FirstStep(x) => match v - x {
+            1 => Day02State::Increasing(v),
+            2 => Day02State::Increasing(v),
+            3 => Day02State::Increasing(v),
+            -1 => Day02State::Decreasing(v),
+            -2 => Day02State::Decreasing(v),
+            -3 => Day02State::Decreasing(v),
+            _ => Day02State::Unsafe,
+        },
+        Day02State::Increasing(x) => match v - x {
+            1 => Day02State::Increasing(v),
+            2 => Day02State::Increasing(v),
+            3 => Day02State::Increasing(v),
+            _ => Day02State::Unsafe,
+        },
+        Day02State::Decreasing(x) => match v - x {
+            -1 => Day02State::Decreasing(v),
+            -2 => Day02State::Decreasing(v),
+            -3 => Day02State::Decreasing(v),
+            _ => Day02State::Unsafe,
+        },
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
