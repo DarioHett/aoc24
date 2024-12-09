bb2151aade9252bc22d874217290298d53d019b4
Dario Hett <dariohett+github@gmail.com>
2024-12-09 20:42:31 +0100

    Day 09: init

---
+++ src/bin/09.rs
@@ -0,0 +1,56 @@
+use anyhow::*;
+use aoc24::*;
+use std::clone::Clone;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "09";
+const INPUT_FILE: &str = "input/09.txt";
+
+const TEST: &str = "00...111...2...333.44.5555.6666.777.888899
+009..111...2...333.44.5555.6666.777.88889.
+0099.111...2...333.44.5555.6666.777.8888..
+00998111...2...333.44.5555.6666.777.888...
+009981118..2...333.44.5555.6666.777.88....
+0099811188.2...333.44.5555.6666.777.8.....
+009981118882...333.44.5555.6666.777.......
+0099811188827..333.44.5555.6666.77........
+00998111888277.333.44.5555.6666.7.........
+009981118882777333.44.5555.6666...........
+009981118882777333644.5555.666............
+00998111888277733364465555.66.............
+0099811188827773336446555566..............";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        Ok(1)
+    }
+
+    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = part1(input_file)?;
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    // println!("\n=== Part 2 ===");
+    //
+    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    //     Ok(1)
+    // }
+    //
+    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    //
+    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    // let result = part2(input_file)?;
+    // println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
