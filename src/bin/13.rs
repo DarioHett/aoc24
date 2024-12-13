3b08b879e58ceee30427afb869b33914d4bb868f
Dario Hett <dariohett+github@gmail.com>
2024-12-13 12:54:20 +0100

    Day 13: solution 01

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,9 +1,8 @@
 use anyhow::*;
-use aoc24::day13::*;
 use aoc24::*;
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::ops::Mul;
 
 const DAY: &str = "13";
 const INPUT_FILE: &str = "input/13.txt";
@@ -24,17 +23,39 @@ Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279";
 
+fn linear_eqn((x1, x2): (i64, i64), (y1, y2): (i64, i64), (z1, z2): (i64, i64)) -> i64 {
+    let b = (z2 * x1 - z1 * x2) / (y2 * x1 - y1 * x2);
+    let a = (z1 - b * y1) / x1;
+    // This should work as any fractional result will be truncated to mismatch.
+    if (x1 * a + y1 * b, x2 * a + y2 * b) != (z1, z2) {
+        return 0;
+    }
+    a * 3 + b
+}
 fn main() -> Result<()> {
     start_day(DAY);
 
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        Ok(1)
+    fn part1<R: BufRead>(mut reader: R) -> Result<i64> {
+        let mut input = &mut "".to_string();
+        let _ = reader.read_to_string(input);
+        let mut res = 0;
+        for l in input.split("\n\n") {
+            // May replace w/ regex
+            let (x1, x2, y1, y2, z1, z2) = l
+                .split(|c: char| !c.is_ascii_digit())
+                .filter(|w| !w.is_empty())
+                .map(|w| w.parse().unwrap())
+                .collect_tuple()
+                .unwrap();
+            res += linear_eqn((x1, x2), (y1, y2), (z1, z2));
+        }
+        Ok(res)
     }
 
-    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+    assert_eq!(480, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = part1(input_file)?;
@@ -44,12 +65,11 @@ fn main() -> Result<()> {
     //region Part 2
     // println!("\n=== Part 2 ===");
     //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //
+    // fn part2<R: BufRead>(mut reader: R) -> Result<i64> {
     //     Ok(1)
     // }
     //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    // // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
     //
     // let input_file = BufReader::new(File::open(INPUT_FILE)?);
     // let result = part2(input_file)?;
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ pub mod day09;
 pub mod day10;
 pub mod day11;
 pub mod day12;
-pub mod day13;
 
 use itertools::{iproduct, Itertools};
 use std::collections::HashMap;
