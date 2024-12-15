362f235031c0822a2310b35f65c1069dd95009bf
Dario Hett <dariohett+github@gmail.com>
2024-12-15 13:20:36 +0100

    Day 15: init

---
+++ src/bin/15.rs
@@ -0,0 +1,61 @@
+use anyhow::*;
+use aoc24::*;
+use std::fs;
+
+const DAY: &str = "15";
+const INPUT_FILE: &str = "input/15.txt";
+
+const TEST: &str = "##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1(input: &str) -> Result<usize> {
+        Ok(1)
+    }
+
+    assert_eq!(10092, part1(TEST)?);
+
+    let input_file = fs::read_to_string(INPUT_FILE)?;
+
+    let result = part1(input_file.as_str())?;
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    // println!("\n=== Part 2 ===");
+    //
+    // fn part2(input: &str) -> Result<usize> {
+    //     Ok(1)
+    // }
+    // //
+    // assert_eq!(0, part2(TEST));;
+    // let res = part2(input_file.as_str())?;
+    // println!("Result = {}", res);
+    //endregion
+
+    Ok(())
+}
