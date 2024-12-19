76d8dad9497922e38296e7b3ffeeaff47254e73e
Dario Hett <dariohett+github@gmail.com>
2024-12-20 00:04:30 +0100

    Day 19: solution 01

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,23 +1,37 @@
 use anyhow::*;
+use aoc24::day11::parse_input;
 use aoc24::day19::*;
-use aoc24::util::grid::Grid;
-use aoc24::util::point::Point;
 use aoc24::*;
 use std::fs;
 
 const DAY: &str = "19";
 const INPUT_FILE: &str = "input/19.txt";
 
-const TEST: &str = "r, wr, b, g, bwu, rb, gb, br
-
-brwrr
-bggr
-gbbr
-rrbgbr
-ubwu
-bwurrg
-brgr
-bbrgwb";
+// Forward pass; abandoned due to complexity (TEST sufficiently small, real data slightly too large)
+fn eval_patterns(cur_soln: &Vec<u8>, design: &Vec<u8>, patterns: &Vec<Vec<u8>>) -> u8 {
+    let start_char = design[cur_soln.len()];
+    // let mut ret_vals = Vec::new();
+    for p in patterns
+        .iter()
+        .filter(|&p| (p.len() + cur_soln.len()) <= design.len())
+        .filter(|&p| p[0] == start_char)
+        .filter(|&p| design[cur_soln.len()..(cur_soln.len()+p.len())].eq(p))
+    {
+        if (cur_soln.len() + p.len() == design.len()) {
+            println!("1- {:?}", design);
+            return 1
+        } else {
+            let mut new_soln = cur_soln.clone();
+            for &c in p {
+                new_soln.push(c);
+            }
+            if eval_patterns(&new_soln, design, patterns) == 1 {
+                return 1
+            }
+        }
+    }
+    return 0
+}
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -25,8 +39,9 @@ fn main() -> Result<()> {
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn part1(input: &str) -> Result<u32> {
-        Ok(1)
+    fn part1(input: &str) -> Result<usize> {
+        let ctr = parse_pt1(input);
+        Ok(ctr)
     }
 
     let input_file = fs::read_to_string(INPUT_FILE)?;
@@ -39,12 +54,12 @@ fn main() -> Result<()> {
     //region Part 2
     // println!("\n=== Part 2 ===");
     // //
-    // fn part2(input: &str) -> Result<u64> {
-    //     Ok(1)
+    // fn part2(input: &str) -> Result<usize> {
+    //     Ok(parse_pt2(input))
     // }
     //
     // //
-    // assert_eq!(0, part2(TEST)?);
+    // assert_eq!(16, part2(TEST)?);
     // let res = part2(input_file.as_str())?;
     // println!("Result = {}", res);
     //endregion
--- src/day19.rs
+++ src/day19.rs
@@ -1,10 +1,218 @@
+use std::char::from_u32;
+
+pub const TEST: &str = "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+
+pub fn parse(input: &str) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
+    let mut ln = input.lines();
+    let patterns = ln
+        .next()
+        .unwrap()
+        .split(", ")
+        .map(|s| s.bytes().collect::<Vec<u8>>())
+        .collect::<Vec<Vec<u8>>>();
+    let desings = ln
+        .skip(1)
+        .map(|s| s.bytes().collect::<Vec<u8>>())
+        .collect::<Vec<Vec<u8>>>();
+    (patterns, desings)
+}
+
+fn is_pattern_valid(suggested_pattern: &str, loc: usize, design: &str) -> bool {
+    if (loc + suggested_pattern.len() > design.len()) {
+        return false;
+    }
+    let pattern_validity = (String::from(suggested_pattern)
+        .as_mut()
+        .chars()
+        .enumerate()
+        .fold(true, |acc, (i, c)| {
+            acc && (design.chars().nth(loc + i).unwrap() == c)
+        }));
+    pattern_validity
+}
+
+fn add_candidate(
+    cand: &Vec<u8>,
+    loc: usize,
+    current_solution: &Vec<u8>,
+    target: &Vec<u8>,
+) -> Option<Vec<u8>> {
+    if (current_solution.len() + cand.len() <= target.len()) {
+        let mut potential_solution = current_solution.clone();
+        potential_solution.resize(current_solution.len() + cand.len(), 0);
+        for (i, &v) in cand.iter().enumerate() {
+            potential_solution[loc + i] = v
+        }
+        Some(potential_solution)
+    } else {
+        None
+    }
+}
+
+fn add_cands(
+    cands: &Vec<Vec<u8>>,
+    loc: usize,
+    current_solution: &Vec<u8>,
+    target: &Vec<u8>,
+) -> Option<Vec<Vec<u8>>> {
+    let mut solns = Vec::new();
+    for c in cands {
+        if let Some(solution) = add_candidate(c, loc, current_solution, target) {
+            solns.push(solution);
+        }
+    }
+    if solns.is_empty() {
+        return None;
+    }
+    Some(solns)
+}
+
+pub fn design_pattern_map(design: &Vec<u8>, patterns: &Vec<Vec<u8>>) -> Vec<Vec<Vec<u8>>> {
+    let mut tbl = Vec::with_capacity(design.len());
+    for (i, &b) in design.iter().enumerate() {
+        let mut lookup = Vec::new();
+        let remaining_bytes = design.len() - i;
+        for p in patterns {
+            if !is_pattern_valid(
+                p.into_iter()
+                    .map(|&e| from_u32(e as u32).unwrap())
+                    .collect::<String>()
+                    .as_str(),
+                i,
+                design
+                    .into_iter()
+                    .map(|&e| from_u32(e as u32).unwrap())
+                    .collect::<String>()
+                    .as_str(),
+            ) {
+                continue;
+            }
+            if p.len() > remaining_bytes {
+                continue;
+            }
+            if p[0] != b {
+                continue;
+            }
+            lookup.push(p.clone());
+        }
+        tbl.push(lookup);
+    }
+    tbl
+}
+
+pub fn run(candidate_sets: &Vec<Vec<Vec<u8>>>, target: &Vec<u8>) -> Vec<Vec<u8>> {
+    let presolns = add_cands(&candidate_sets[0], 0, &Vec::new(), target);
+    if presolns.is_none() {
+        return vec![];
+    }
+    let mut solns = presolns.unwrap();
+    for i in 1..candidate_sets.len() {
+        let mut sol_buffer = Vec::new();
+        for s in solns {
+            if s.len() == i {
+                let additional_solns = add_cands(&candidate_sets[i], i, &s.clone(), target);
+                if additional_solns.is_some() {
+                    for t in additional_solns.unwrap() {
+                        sol_buffer.push(t.clone());
+                    }
+                }
+            } else {
+                sol_buffer.push(s)
+            }
+        }
+        solns = sol_buffer;
+    }
+    solns
+}
+
+pub fn parse_pt1(input: &str) -> usize {
+    let (patterns, designs) = input.split_once("\n\n").unwrap();
+    let mut lookup = Vec::new();
+    lookup.push(NextCell::new());
+
+    for pattern in patterns.split(", ") {
+        let mut i = 0;
+
+        for j in pattern.bytes().map(compress) {
+            if lookup[i].next[j] == 0 {
+                lookup[i].next[j] = lookup.len();
+                i = lookup.len();
+                lookup.push(NextCell::new());
+            } else {
+                i = lookup[i].next[j];
+            }
+        }
+
+        lookup[i].set_pattern();
+    }
+
+    let mut ctr = 0;
+    let mut ways = Vec::new();
+
+    for design in designs.lines().map(str::as_bytes) {
+        let size = design.len();
+
+        ways.clear();
+        ways.resize(size + 1, 0);
+        ways[0] = 1;
+
+        for start in 0..size {
+            if ways[start] > 0 {
+                let mut i = 0;
+
+                for end in start..size {
+                    i = lookup[i].next[compress(design[end])];
+                    if i == 0 {
+                        break;
+                    }
+                    ways[end + 1] += lookup[i].pattern() * ways[start];
+                }
+            }
+        }
+
+        let total = ways[size];
+        ctr += (total > 0) as usize;
+    }
+
+    ctr
+}
+
+fn compress(b: u8) -> usize {
+    let n = b as usize;
+    (n ^ (n >> 4)) % 8
+}
+
+struct NextCell {
+    next: [usize; 6],
+}
+
+impl NextCell {
+    fn new() -> Self {
+        NextCell { next: [0; 6] }
+    }
+
+    fn set_pattern(&mut self) {
+        self.next[3] = 1;
+    }
+
+    fn pattern(&self) -> usize {
+        self.next[3]
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-
     #[test]
-    fn test() {
-    }
+    fn test() {}
 }
