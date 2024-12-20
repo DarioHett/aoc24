c5563b664b97053d1f2f7bb2514880355e9c0364
Dario Hett <dariohett+github@gmail.com>
2024-12-20 22:16:45 +0100

    Day 20: solution 02

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -4,7 +4,6 @@ use aoc24::util::point::{Point, DOWN, LEFT, RIGHT, UP};
 use aoc24::*;
 use itertools::Itertools;
 use std::fs;
-use std::path::absolute;
 
 const DAY: &str = "20";
 const INPUT_FILE: &str = "input/20.txt";
@@ -103,19 +102,79 @@ fn main() -> Result<()> {
     println!("Result = {}", result);
     //endregion
 
-    // //region Part 2
-    // println!("\n=== Part 2 ===");
-    // //
-    // fn part2(input: &str) -> Result<usize> {
-    //
-    //     Ok(1)
-    // }
-    //
-    // //
+    //region Part 2
+    println!("\n=== Part 2 ===");
+
+    fn part2(input: &str) -> Result<usize> {
+        let move_grid = Grid::parse(input);
+        let time_grid = &mut move_grid.same_size_with(usize::MAX);
+
+        let mut pos = move_grid.find(b'S').unwrap();
+        let fin = move_grid.find(b'E').unwrap();
+
+        // Initial parameters
+        let mut cum_time = 0;
+        let mut dir = if move_grid[pos + UP] != b'#' {
+            UP
+        } else if move_grid[pos + DOWN] != b'#' {
+            DOWN
+        } else if move_grid[pos + LEFT] != b'#' {
+            LEFT
+        } else {
+            RIGHT
+        };
+
+        // Initialize time_grid
+        while pos != fin {
+            time_grid[pos] = cum_time;
+            pos = pos + dir;
+            cum_time += 1;
+
+            for d in [dir.clockwise(), dir, dir.counter_clockwise()] {
+                if move_grid[pos + d] != b'#' {
+                    dir = d;
+                    break;
+                }
+            }
+        }
+        time_grid[pos] = cum_time; // Missed this one
+
+        // Find diffs where either horizontally or vertically 2 steps are > 100
+        let mut ctr = 0;
+        let dirs = (-20i32..21i32)
+            .map(|x| {
+                ((-20i32 + x.abs())..(21i32 - x.abs()))
+                    .map(|y| Point::new(x, y))
+                    .collect::<Vec<_>>()
+            })
+            .flatten()
+            .filter(|pt| (pt.x.abs() + pt.y.abs()) > 1)
+            .collect::<Vec<_>>();
+        for (x, y) in (0..move_grid.width).cartesian_product(0..move_grid.height) {
+            let pt = Point::new(x, y);
+            if (move_grid[pt] == b'#') {
+                continue;
+            };
+            for &dir in dirs.iter() {
+                if move_grid.contains(pt + dir) {
+                    if (move_grid[pt + dir] != b'#') {
+                        let saving = (time_grid[pt].abs_diff(time_grid[pt + dir])
+                            - (dir.x.abs() + dir.y.abs()) as usize);
+                        // println!("Found: {:?}:{:?}:{:?}", pt, pt+dir, saving);
+                        if saving >= 100 {
+                            ctr += 1;
+                        }
+                    };
+                }
+            }
+        }
+
+        Ok(ctr / 2)
+    }
     // assert_eq!(0, part2(TEST)?);
-    // let res = part2(input_file.as_str())?;
-    // println!("Result = {}", res);
-    // //endregion
+    let res = part2(input_file.as_str())?;
+    println!("Result = {}", res);
+    //endregion
 
     Ok(())
 }
